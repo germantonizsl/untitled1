86b2419d15ffbe9a5f305ff1f2bd4a5b77a4ad7a
germantonizsl <germanovich_anton@mail.ru>
2024-01-12 23:02:17 +0300

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public  static void main(String[] args) {
 
         Scanner scanner=new Scanner(System.in);
+        // fisrt
         System.out.println("ПЕРВОЕ ЧИСЛО");
         int n1=scanner.nextInt();
         System.out.println("Второе ЧИСЛО");
